68dc7e6ed25dba3e931a8327d68b597e8ba708e4
PlatonCraft <platoncrafter@gmail.com>
2016-02-13 22:42:57 +0300

    [Interface] Изменён метод отрисовки [Control] Добавлено управление стрелками (почему нет?)

--- FSMForm.cs
+++ FSMForm.cs
@@ -12,8 +12,6 @@ namespace FSMForm
 {
     public partial class FSMForm : Form
     {
-        Machine machine = new Machine();
-
         public FSMForm()
         {
             InitializeComponent();
@@ -26,8 +24,15 @@ namespace FSMForm
 
         private void FSMForm_Load(object sender, EventArgs e)
         {
-            machine.Init();
+            this.KeyDown += new KeyEventHandler(KeyDownEventHandler);
+
+            Machine.Init();
             RegisterPictures(Interface.picList);
+        }
+
+        private void KeyDownEventHandler(object sender, KeyEventArgs e)
+        {
+            Robot.RobotController(Machine.rob, e.KeyCode.ToString());
         }
 
         /// <summary>
@@ -54,11 +59,11 @@ namespace FSMForm
         /// <param name = "Step">"Человеческая" координата.</param>
         private struct PosInfo
         {
-            public int TxtScl, Step, Min, Max, Zero;
+            public int Scale, Step, Min, Max, Zero;
             
             public PosInfo(int scl, int stp, int min, int max, int zr)
             {
-                TxtScl = scl; //Texture Scale - масштабирование текстуры
+                Scale = scl; //Texture Scale - масштабирование текстуры
                 Step = stp * scl; Min = min * scl; Max = max * scl; Zero = zr * scl;
             }
         }
@@ -79,26 +84,14 @@ namespace FSMForm
         }
 
         /// <summary>
-        /// Метод для отрисовки картинки на заданных координатах.
+        /// Метод для отрисовки картинки объекта на заданных координатах.
         /// </summary>
-        public static void DrawOnCoords(Picture pic, int coordX, int coordY)
+        public static void DrawObject(MovingObject obj)
         {
-           if (CoordToPos(X, coordX) <= X.Max && CoordToPos(X, coordX) >= X.Min)
-                pic.Left = CoordToPos(X, coordX);
-           if (CoordToPos(Y, coordY) <= Y.Max && CoordToPos(Y, coordY) >= Y.Min)
-                pic.Top = CoordToPos(Y, coordY);
-        }
-
-        /// <summary>
-        /// Метод для смещения картинки на заданные координаты.
-        /// </summary>
-        public static void DrawOnOffset(Picture pic, int offsetX, int offsetY)
-        {
-            
-            if (pic.Left + offsetX * X.Step <= X.Max && pic.Left + offsetX * X.Step >= X.Min)
-                pic.Left += offsetX * X.Step;
-            if (pic.Top + offsetY * Y.Step <= Y.Max && pic.Top + offsetY * Y.Step >= Y.Min)
-                pic.Top += offsetY * Y.Step;
+           if (CoordToPos(X, obj.coordX) <= X.Max && CoordToPos(X, obj.coordX) >= X.Min)
+                obj.pic.Left = CoordToPos(X, obj.coordX) - obj.width * X.Scale / 2;
+           if (CoordToPos(Y, obj.coordY) <= Y.Max && CoordToPos(Y, obj.coordY) >= Y.Min)
+                obj.pic.Top = CoordToPos(Y, obj.coordY) - obj.height * Y.Scale / 2;
         }
 
         /// <summary>
@@ -106,8 +99,7 @@ namespace FSMForm
         /// </summary>
         public static void SetObjPicture(MovingObject obj)
         {
-            picList.Add(new Picture(obj.name, obj.id, obj.width * X.TxtScl, obj.height * Y.TxtScl,
-                CoordToPos(X, obj.coordX) - obj.width * X.TxtScl / 2, CoordToPos(Y, obj.coordY) - obj.height * Y.TxtScl / 2));
+            picList.Add(new Picture(obj.name, obj.id, obj.width * X.Scale, obj.height * Y.Scale));
             obj.pic = picList[picList.Count-1];
         }
     }
@@ -115,26 +107,25 @@ namespace FSMForm
     /// <summary>
     /// Главный класс конечного автомата.
     /// </summary>
-    class Machine
+    static class Machine
     {
-        public Robot rob;
-        public Target tar;
-
-        public Machine() { }
+        public static Robot rob;
+        public static Target tar;
 
         /// <summary>
         /// Выполняется при инициализации конечного автомата.
         /// Здесь создаются и инициализируются двигающиеся объекты.
         /// </summary>
-        public void Init() {
+        public static void Init() {
             rob = new Robot("Savvy", 0);
             tar = new Target("Portal", 0);
             Interface.SetObjPicture(rob);
-            rob.pic.BackColor = System.Drawing.Color.Red;
+            Interface.DrawObject(rob);
             Interface.SetObjPicture(tar);
+            Interface.DrawObject(tar);
+            rob.pic.BackColor = System.Drawing.Color.Red;
             tar.pic.BackColor = System.Drawing.Color.Green;
         }
-
     }
 
     /// <summary>
@@ -164,6 +155,64 @@ namespace FSMForm
             this.coordY = 0;
             this.width = this.height = 6;
         }
+
+        /// <summary>
+        /// Статический метод для управления роботом.
+        /// </summary>
+        public static void RobotController(Robot rob, string key)
+        {
+            switch (key)
+            {
+                case "Up":
+                    rob.MoveUp();
+                    break;
+
+                case "Down":
+                    rob.MoveDown();
+                    break;
+
+                case "Left":
+                    rob.MoveLeft();
+                    break;
+
+                case "Right":
+                    rob.MoveRight();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public void MoveUp()
+        {
+            if(coordY + 1 <= 8)
+                this.coordY++;
+            Interface.DrawObject(this);
+        }
+        public void MoveDown()
+        {
+            if (coordY - 1 >= -8)
+            {
+                this.coordY--;
+                Interface.DrawObject(this);
+            }
+        }
+        public void MoveLeft()
+        {
+            if (coordX - 1 >= -8)
+            {
+                this.coordX--;
+                Interface.DrawObject(this);
+            }
+        }
+        public void MoveRight()
+        {
+            if (coordX + 1 <= 8)
+            {
+                this.coordX++;
+                Interface.DrawObject(this);
+            }
+        }
     }
 
     class Target : MovingObject
@@ -172,8 +221,8 @@ namespace FSMForm
         {
             this.name = name;
             this.id = id;
-            this.coordX = 0;
-            this.coordY = 0;
+            this.coordX = -3;
+            this.coordY = 4;
             this.width = this.height = 8;
         }
     }
@@ -192,11 +241,10 @@ namespace FSMForm
         /// <param name = "height">Высота картинки.</param>
         /// <param name = "posX">Пиксельная координата X центра картинки.</param>
         /// <param name = "posY">Пиксельная координата Y центра картинки.</param>
-        public Picture(string name, int id, int width, int height, int posX, int posY)
+        public Picture(string name, int id, int width, int height)
         {
             this.Name = name + " " + id;
             this.Size = new System.Drawing.Size(width, height);
-            this.Location = new System.Drawing.Point(posX, posY);
         }
     }
 }
